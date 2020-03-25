bc6eff680f7e6c43dc1382b6f3ca777f81123c9e
Isintume Jennifer <isintumejenny@gmail.com>
2020-03-26 00:08:35 +0100

    add assignmentjs.js

---
+++ assignmentjs.js
@@ -0,0 +1,5 @@
+var myName = "Jennifer";
+ var myCourses = ["html", "css", "javascript", "Node.js"]
+for(var myCourses = 0; myCourses <= 200; myCourses++){if(myCourses%2 == 0){
+	console.log(myCourses);}
+}

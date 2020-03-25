3213a33cbebaea22b75bcf2b280b8014eefe4b53
Isintume Jennifer <isintumejenny@gmail.com>
2020-03-25 13:22:57 +0100

    added calculator route

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3005;
 
 app.get('/', (req, res)=>{
-    res.send("Welcome to the Homepage");
+    res.send(`Welcome to the Homepage - <a href="/user">Go to Profile</a>`);
+});
+
+app.get('/user', function(req, res) {
+    res.send('My name is Nkem. This is my profile page - <a href="/">Go Home</a>');
+});
+
+app.get('/calculator/add', (req, res)=>{
+    res.send("This is the calculator page");
 });
 
 try {
